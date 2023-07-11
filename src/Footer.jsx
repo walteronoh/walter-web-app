bb65f1839b6982dd85e6faf4898fb286dc756faf
maninthehood <115598773+maninthehood@users.noreply.github.com>
2023-07-11 03:08:38 +0000

    done

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,8 @@ export default function Footer() {
   return (
     <>
 
-      <footer class="bg-white dark:bg-gray-900">
+      <footer class="bg-white dark:bg-gray-900
+        ">
         <div class="mx-auto  w-full max-w-screen-xl p-4 py-6 lg:py-8">
           <div class="md:flex md:justify-between">
             <div class="mb-6 md:mb-0">
