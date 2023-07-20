83626369f477e0930aea288000c8b5e25ca66256
carlos <115598773+maninthehood@users.noreply.github.com>
2023-07-20 04:37:40 -0400

    Update Form.jsx

--- src/Pages/Form.jsx
+++ src/Pages/Form.jsx
@@ -1,27 +1,32 @@
-import { useRef, useState } from "react"
-import emailjs from '@emailjs/browser';
+import React, { useRef, useState } from "react";
+import emailjs from 'emailjs-com';
 
 export default function Form() {
   const form = useRef();
   const [isSending, setIsSending] = useState(false);
 
-  const sendMail = (e) => {
-    e.preventDefault();
-    try {
-      setIsSending(true);
-      emailjs.sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, form.current, import.meta.env.VITE_PUBLIC_KEY)
-        .then((result) => {
-          setIsSending(false);
-          alert('Message sent successfully.');
-        }, (error) => {
-          setIsSending(false);
-          alert('It seems like there was an error while sending your message.');
-        });
-    } catch (error) {
-      setIsSending(false);
-      alert('It seems like there was an error while sending your message.');
-    }
-  };
+const sendMail = (e) => {
+  e.preventDefault();
+  try {
+    setIsSending(true);
+    emailjs.sendForm(
+      import.meta.env.SERVICE_ID,
+      import.meta.env.TEMPLATE_ID,
+      form.current,
+      import.meta.env.USER_ID
+    )
+      .then((result) => {
+        setIsSending(false);
+        alert('Message sent successfully.');
+      }, (error) => {
+        setIsSending(false);
+        alert('It seems like there was an error while sending your message.');
+      });
+  } catch (error) {
+    setIsSending(false);
+    alert('It seems like there was an error while sending your message.');
+  }
+};
 
   return (
     <section className="bg-white dark:bg-gray-900">
@@ -30,7 +35,7 @@ export default function Form() {
         <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Let me know.</p>
         <form ref={form} className="space-y-8" onSubmit={sendMail}>
           <div>
-            <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your name</label>
+            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your name</label>
             <input type="text" name="from_name" id="from_name" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name" required />
           </div>
           <div className="sm:col-span-2">
@@ -39,7 +44,7 @@ export default function Form() {
           </div>
           {isSending ?
             <div role="status">
-              <svg aria-hidden="true" class="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg aria-hidden="true" className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                 <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
                 <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
               </svg>
