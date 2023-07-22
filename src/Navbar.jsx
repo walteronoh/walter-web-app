2340a8b35e66f1788d3808eb43cbc55b3a8b4d98
carloskirui <115598773+maninthehood@users.noreply.github.com>
2023-07-22 03:52:34 -0400

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
+import { Link } from 'react-router-dom
+  
 function NavBar() {
+  const [showMenu, setShowMenu] = React.useState(false);
+
+  function toggleMenu() {
+    setShowMenu(!showMenu);
+  }
+
   return (
     <>
       <nav className="bg-white dark:bg-gray-900  border-b-4 border-blue-900 ">
@@ -17,11 +23,9 @@ function NavBar() {
             </span>
           </a>
           <button
-            data-collapse-toggle="navbar-default"
             type="button"
             className="inline-flex items-center duration-500 p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-default"
-            aria-expanded="false"
+            onClick={toggleMenu}
           >
             <span className="sr-only">Open main menu</span>
             <svg
@@ -32,13 +36,13 @@ function NavBar() {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               ></path>
             </svg>
           </button>
-          <div className="hidden w-full md:block md:w-auto duration-500 truncate" id="navbar-default">
+          <div className={`w-full md:block md:w-auto duration-500 truncate ${showMenu ? 'block' : 'hidden'}`} id="navbar-default">
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border duration-500 border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <Link
@@ -67,6 +71,3 @@ function NavBar() {
     </>
   );
 }
-
-
-export default NavBar;
